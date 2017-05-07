fd3cd16455432c74d86d7ae67af62eb5bc8dbad3
Jufebrown <jufebrown@gmail.com>
2017-05-07 03:18:52 -0500

    set up dev database

--- knexfile.js
+++ knexfile.js
@@ -12,23 +12,17 @@ module.exports = {
       directory: __dirname + '/db/seeds/test'
     }
   },
-
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
+  development: {
+    client: 'pg',
+    debug: true,
+    connection: 'postgres://localhost/abc',
     migrations: {
-      tableName: 'knex_migrations'
+      directory: __dirname + '/db/migrations'
+    },
+    seeds: {
+      directory: __dirname + '/db/seeds/development'
     }
   },
-
   production: {
     client: 'postgresql',
     connection: {
