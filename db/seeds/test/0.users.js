382f28bc16fba589f45ccb43fa344e113c7c5bec
Jufebrown <jufebrown@gmail.com>
2017-05-08 15:33:20 -0500

    changed users seed to use hashed password

--- db/seeds/test/0.users.js
+++ db/seeds/test/0.users.js
@@ -1,10 +1,19 @@
+`use strict`
+
 const users = require('../users')
+const bcrypt = require('bcryptjs');
 
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('users').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('users').insert(users);
-    });
+  .then(() => {
+    const salt = bcrypt.genSaltSync();
+    const hash = bcrypt.hashSync('johnson123', salt);
+    return Promise.join(
+      knex('users').insert({
+        username: 'jeremy',
+        password: hash
+      })
+    );
+  });
 };
