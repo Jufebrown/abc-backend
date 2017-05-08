9e36aaab2d0c84b20d4c9928245dd5731e263ded
Jufebrown <jufebrown@gmail.com>
2017-05-08 18:11:47 -0500

    changed authenticated to makeSureAuthenticated

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -2,11 +2,11 @@
 
 const express = require('express')
 const router = express.Router()
-const {getUserGames, register, login, authenticated} = require('../controllers/authCtrl')
+const {getUserGames, register, login, makeSureAuthenticated} = require('../controllers/authCtrl')
 
 router.post('/auth/register', register)
 router.post('/auth/login', login)
 router.get('/auth/games', getUserGames)
-router.get('/auth/user', authenticated)
+router.get('/auth/user', makeSureAuthenticated)
 
 module.exports = router
