69e3b8eacd0ad8fab5bcde1df42f7ede83775a9b
Jufebrown <jufebrown@gmail.com>
2017-05-07 22:41:42 -0500

    added users_friends seed

---
+++ db/seeds/development/6.users_friends.js
@@ -0,0 +1,10 @@
+const users_friends = require('../users_friends')
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('users_friends').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users_friends').insert(users_friends);
+    });
+};
