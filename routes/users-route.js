564924aa536119b184233054c39baa1511a4da68
Jufebrown <jufebrown@gmail.com>
2017-05-08 12:37:25 -0500

    added route for getting games for logged in user to user-route

--- routes/users-route.js
+++ routes/users-route.js
@@ -1,1 +1,10 @@
 `use strict`
+
+const {Router} = require('express')
+const router = Router()
+
+const {getGamesAll} = require('../controllers/usersCtrl')
+
+router.get('/users/games', getUserGames)
+
+module.exports = router
