6a9dc85625f7a54999532fe2284716351aee0029
Jufebrown <jufebrown@gmail.com>
2017-05-08 10:08:00 -0500

    commented out unfinished routes

--- routes/index.js
+++ routes/index.js
@@ -3,16 +3,16 @@
 const { Router } = require('express')
 const router = Router()
 
-router.use(require('./users-route'))
+// router.use(require('./users-route'))
 router.use(require('./games-route'))
-router.use(require('./words-route'))
-router.use(require('./friends-route'))
+// router.use(require('./words-route'))
+// router.use(require('./friends-route'))
 router.get('/',function (req,res) {
   res.json ({
-    "users": "/api/v1/users",
-    "games" : "/api/v1/games",
-    "words" : "/api/v1/words",
-    "friends": "/api/v1/friends"
+    // "users": "/api/v1/users",
+    "games" : "/api/v1/games"
+    // "words" : "/api/v1/words",
+    // "friends": "/api/v1/friends"
   })
 })
 
