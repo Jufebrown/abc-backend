cabdc8e39df8ff7dde153f4deea42b59d25b85ce
Jufebrown <jufebrown@gmail.com>
2017-05-08 12:24:31 -0500

    added test for getting games associated with logged in user

--- test/route-spec.js
+++ test/route-spec.js
@@ -50,4 +50,19 @@ describe('abc routes', ()=>{
         })
     })
   })
+
+  // tests getting all games for logged in user
+  describe(`GET /api/v1/games`, function() {
+    it(`should return all games`, function() {
+      return chai.request(server)
+        .get(`/api/v1/games/user="1"`).then(res => {
+          res.should.have.status(200)
+          res.should.be.json
+          res.body.should.be.a.object
+          res.body.should.have.key('games')
+          res.body.games.should.be.a.array
+          res.body.games[0].number_correct.should.equal('4')
+        })
+    })
+  })
 })
