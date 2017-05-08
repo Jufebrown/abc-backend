bfbaea6fb404631ae4bb9c1b4083ba39ed5bb10a
Jufebrown <jufebrown@gmail.com>
2017-05-08 14:37:01 -0500

    added createUser method to Auth model

---
+++ models/auth.js
@@ -0,0 +1,17 @@
+const bcrypt = require('bcryptjs');
+const knex = require('../db/connection');
+
+function createUser(req) {
+  const salt = bcrypt.genSaltSync();
+  const hash = bcrypt.hashSync(req.body.password, salt);
+  return knex('users')
+  .insert({
+    username: req.body.username,
+    password: hash
+  })
+  .returning('*');
+}
+
+module.exports = {
+  createUser
+};
