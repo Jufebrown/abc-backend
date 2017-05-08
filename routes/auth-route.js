22603d2661a12c193530c9f28f1a8841e78d2b6d
Jufebrown <jufebrown@gmail.com>
2017-05-08 15:49:35 -0500

    added login route to auth routes

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -22,4 +22,26 @@ router.post('/auth/register', (req, res, next)  => {
   });
 });
 
+router.post('/auth/login', (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  return Auth.getUser(username)
+  .then((response) => {
+    Auth.comparePass(password, response.password);
+    return response;
+  })
+  .then((response) => { return localAuth.encodeToken(response); })
+  .then((token) => {
+    res.status(200).json({
+      status: 'success',
+      token: token
+    });
+  })
+  .catch((err) => {
+    res.status(500).json({
+      status: 'error'
+    });
+  });
+});
+
 module.exports = router;
