4942c3a4a4e9112d554d4f5c00808213feaa9c16
Jufebrown <jufebrown@gmail.com>
2017-05-08 12:24:03 -0500

    added user model

--- models/user.js
+++ models/user.js
@@ -1,1 +1,21 @@
-`use strict`
+const {bookshelf} = require('../db/database')
+// require('./userFriend')
+// require('./friend')
+require('./game')
+require('./userGame')
+
+const User = bookshelf.Model.extend({
+  tableName: 'users',
+  // sets relationship with games
+  game: function () { return this.belongsToMany('Game').through('userGame')}
+  // sets relationship with friends
+  // friend: function () { return this.belongsToMany('Friend').through('userFriend')},
+}, {
+  // gets single user
+  getSingleUser: function(id) {
+    return this.forge().fetchAll()
+  }
+
+})
+
+module.exports = bookshelf.model('User', User)
