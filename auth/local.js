5b2648afc35fdc389e0c4b3e5248b84d1088e2a0
Jufebrown <jufebrown@gmail.com>
2017-05-08 14:23:28 -0500

    changed playload to payload and added decode function

--- auth/local.js
+++ auth/local.js
@@ -14,7 +14,15 @@ function encodeToken(user) {
     // subject of token
     sub: user.id
   };
-  return jwt.encode(playload, process.env.TOKEN_SECRET);
+  return jwt.encode(payload, process.env.TOKEN_SECRET);
+}
+
+function decodeToken(token, callback) {
+  const payload = jwt.decode(token, process.env.TOKEN_SECRET);
+  const now = moment().unix();
+  // check if the token has expired
+  if (now > payload.exp) callback('Token has expired.');
+  else callback(null, payload);
 }
 
 module.exports = {
