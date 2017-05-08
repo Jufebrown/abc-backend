2fddc599d0326c10a7dac054b94557828adc0789
Jufebrown <jufebrown@gmail.com>
2017-05-07 22:45:24 -0500

    copied development seeds into test seeds

---
+++ db/seeds/test/5.games_words.js
@@ -0,0 +1,10 @@
+const games_words = require('../games_words')
+
+exports.seed = function(knex, Promise) {
+  // Deletes ALL existing entries
+  return knex('games_words').del()
+    .then(function () {
+      // Inserts seed entries
+      return knex('games_words').insert(games_words);
+    });
+};
