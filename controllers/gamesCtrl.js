2fad781247c3515954afaee4a9ee3e7435225aa8
Jufebrown <jufebrown@gmail.com>
2017-05-08 11:36:48 -0500

    changed method name so controller and model wouldn't have same method name

--- controllers/gamesCtrl.js
+++ controllers/gamesCtrl.js
@@ -5,7 +5,7 @@ const Game = require('../models/game')
 
 // Fetches games from database when called
 // responds with status code 200 and json of games if successful
-module.exports.getGames = (req, res, next) => {
+module.exports.getGamesAll = (req, res, next) => {
   Game.getAllGames()
     .then(games => res.status(200).json({games}))
     .catch(error => next(error))
