ddaea47d59b0951a9f4776cadf086816a8ff3b20
Jufebrown <jufebrown@gmail.com>
2017-05-08 14:22:37 -0500

    added console.log and comments

--- auth/local.js
+++ auth/local.js
@@ -5,9 +5,13 @@ const moment = require('moment');
 const jwt = require('jwt-simple');
 
 function encodeToken(user) {
-  const playload = {
+  console.log('TOKEN_SECRET', process.env.TOKEN_SECRET)
+  const payload = {
+    // expiration date of token
     exp: moment().add(14, 'days').unix(),
+    // time token is generated
     iat: moment().unix(),
+    // subject of token
     sub: user.id
   };
   return jwt.encode(playload, process.env.TOKEN_SECRET);
