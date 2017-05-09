ad74e9ca0e863f2fafb04c06c75b17ff21c77b87
Jufebrown <jufebrown@gmail.com>
2017-05-09 17:38:22 -0500

    added relations to word model in game model

--- models/game.js
+++ models/game.js
@@ -1,17 +1,17 @@
 'use strict'
 
 const {bookshelf} = require('../db/database')
-// require('./gameWord')
-// require('./word')
+require('./gameWord')
+require('./word')
 require('./auth')
 require('./userGame')
 
 const Game = bookshelf.Model.extend({
   tableName: 'games',
   // sets relationship with users
-  users: function () { return this.belongsToMany('Auth').through('UserGame')}
+  users: function () { return this.belongsToMany('Auth').through('UserGame')},
   // sets relationship with words
-  // words: function () { return this.belongsToMany('Word').through('gameWord')},
+  words: function () { return this.belongsToMany('Word').through('gameWord')}
 }, {
   // gets all games
   getAllGames: function() {
