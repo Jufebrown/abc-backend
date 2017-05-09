9e642fd5fac866cf3b26709fffc7e07e9e4bb5bc
Jufebrown <jufebrown@gmail.com>
2017-05-09 11:05:06 -0500

    required auth model and called ensureAuthenticate before getting games

--- routes/games-route.js
+++ routes/games-route.js
@@ -2,9 +2,10 @@
 
 const {Router} = require('express')
 const router = Router()
+const Auth = require('../models/auth')
 
 const {getGamesAll} = require('../controllers/gamesCtrl')
 
-router.get('/games', getGamesAll)
+router.get('/games', Auth.ensureAuthenticated, getGamesAll)
 
 module.exports = router
