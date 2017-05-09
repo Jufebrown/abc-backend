2edc43a549c0212f03624b76adb305f7148fefae
Jufebrown <jufebrown@gmail.com>
2017-05-09 17:40:14 -0500

    changed route names in test to get it to pass

--- test/route-spec.js
+++ test/route-spec.js
@@ -57,7 +57,7 @@ describe('abc routes', ()=>{
           res.should.have.status(200)
           res.should.be.json
           res.should.be.a.object
-          res.body.should.have.key(['games','gamesForLoggedInUser', 'register', 'login', 'user', 'friendsForLoggedInUser'])
+          res.body.should.have.key(['getAllgames','getGamesForLoggedInUser', 'register', 'login', 'getLoggedInUser', 'getFriendsForLoggedInUser', 'getWordsForSpecifiedGame'])
         })
     })
   });
