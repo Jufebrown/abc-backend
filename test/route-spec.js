09c1d4e942e1cc8e28be9397cb327a36a4d878f2
Jufebrown <jufebrown@gmail.com>
2017-05-09 11:05:35 -0500

    changed games test to use auth

--- test/route-spec.js
+++ test/route-spec.js
@@ -159,8 +159,17 @@ describe('abc routes', ()=>{
   // tests getting all games
   describe(`GET /api/v1/games`, function() {
     it(`should return all games`, function() {
-      return chai.request(server)
-        .get(`/api/v1/games`).then(res => {
+      chai.request(server)
+      .post('/api/v1/auth/login')
+      .send({
+        username: 'jufe',
+        password: 'password'
+      })
+      .end((error, response) => {
+        should.not.exist(error)
+        chai.request(server)
+        .get(`/api/v1/games`)
+        .then(res => {
           res.should.have.status(200)
           res.should.be.json
           res.body.should.be.a.object
@@ -168,6 +177,18 @@ describe('abc routes', ()=>{
           res.body.games.should.be.a.array
           res.body.games[0].number_correct.should.equal('4')
         })
+      })
+    })
+    it('should throw an error if a user is not logged in', (done) => {
+      chai.request(server)
+      .get('/api/v1/auth/user')
+      .end((err, res) => {
+        should.exist(err)
+        res.status.should.eql(400)
+        res.type.should.eql('application/json')
+        res.body.status.should.eql('Please log in')
+        done()
+      })
     })
   })
 
