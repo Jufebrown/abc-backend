86475745f69b25e6bdb0f847c2fa5d9e94a7f8f6
Jufebrown <jufebrown@gmail.com>
2017-05-10 09:34:14 -0500

    added getWord route to word-route

--- routes/words-route.js
+++ routes/words-route.js
@@ -1,1 +1,10 @@
 `use strict`
+
+const {Router} = require('express')
+const router = Router()
+const Auth = require('../models/auth')
+const {getWord} = require('../controllers/wordsCtrl')
+
+router.get('/word', Auth.ensureAuthenticated, getWord)
+
+module.exports = router
