f2915b8c0b9f2957ee420e518c3e48fbeb17e8aa
Jufebrown <jufebrown@gmail.com>
2017-05-10 13:06:19 -0500

    added route for adding game

--- routes/games-route.js
+++ routes/games-route.js
@@ -3,9 +3,11 @@
 const {Router} = require('express')
 const router = Router()
 const Auth = require('../models/auth')
-const {getGamesAll, getGameWords} = require('../controllers/gamesCtrl')
+const {getGamesAll, getGameWords, addGame} = require('../controllers/gamesCtrl')
 
 router.get('/games', Auth.ensureAuthenticated, getGamesAll)
 router.get('/games/words', Auth.ensureAuthenticated, getGameWords)
+router.post('/games/new', Auth.ensureAuthenticated, addGame)
+
 
 module.exports = router
