8fc71dbb1c6f153cdd8d7ab2a60598f8f274b576
Jufebrown <jufebrown@gmail.com>
2017-05-10 09:49:07 -0500

    added getWord method to wordsCtrl

--- controllers/wordsCtrl.js
+++ controllers/wordsCtrl.js
@@ -1,3 +1,13 @@
 'use strict'
 
 const Word = require('../models/word')
+
+module.exports.getWord = ({params: {correct_word}}, res, next) => {
+  Word.getSingleWord(correct_word)
+  .then( (word) => {
+    res.status(200).json(word)
+  })
+  .catch( (error) => {
+    next(error);
+  });
+};
