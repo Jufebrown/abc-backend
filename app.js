a1a70e6d2b0ee1512b4e89b86461a6011b54824e
Jufebrown <jufebrown@gmail.com>
2017-05-12 14:00:00 -0500

    put cors plugin back into app

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const express = require('express');
 const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
+const cors = require('cors')
 const routes = require('./routes/');
 
 const app = express();
+app.use(cors())
 // This 'if' statement prevents application log messages from
 // displaying in the stdout when the tests are run
 if (process.env.NODE_ENV !== 'test') {
@@ -16,11 +18,11 @@ if (process.env.NODE_ENV !== 'test') {
 
 // *** cross domain requests *** //
 const allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-};
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE')
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  next()
+}
 
 app.use(allowCrossDomain);
 
