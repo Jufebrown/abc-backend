58e15d28e887beff7d1ef638da22fb1eb2e5e761
Jufebrown <jufebrown@gmail.com>
2017-05-18 15:13:47 -0500

    added comments

--- controllers/authCtrl.js
+++ controllers/authCtrl.js
@@ -6,15 +6,19 @@ const localAuth = require('../auth/local')
 const {knex} = require('../db/database')
 const Friend = require('../models/friend')
 
-
+// gets games for a specified user
 module.exports.getUserGames = (req, res, next) => {
+  // breaks headers into an array
   let header = req.headers.authorization.split(' ')
+  // gets token from header array
   let token = header[1]
+  // decodes token and gets user id
   localAuth.decodeToken(token, (err, payload) => {
     return knex('users').where({id: parseInt(payload.sub)}).first()
     .then((user) => {
       const id = user.id
       Auth.forge({id})
+      //gets user info and games
       .fetch({withRelated: ['games'], require: true})
       .then((usergames) => {
         res.status(200).json(usergames)
@@ -26,6 +30,7 @@ module.exports.getUserGames = (req, res, next) => {
   })
 }
 
+// gets friends that a user has unlocked
 module.exports.getUserFriends = (req, res, next) => {
   let header = req.headers.authorization.split(' ')
   let token = header[1]
@@ -45,6 +50,7 @@ module.exports.getUserFriends = (req, res, next) => {
   })
 }
 
+// registers a new user and returns a token
 module.exports.register = (req, res, next) => {
   Auth.createUser(req)
   .then((user) => { return localAuth.encodeToken(user[0]) })
@@ -61,6 +67,7 @@ module.exports.register = (req, res, next) => {
   })
 }
 
+// logs in a user and sends a token
 module.exports.login = (req, res, next) => {
   const username = req.body.username
   const password = req.body.password
@@ -83,6 +90,7 @@ module.exports.login = (req, res, next) => {
   })
 }
 
+// sends success response if user is authenticated (called from Auth model)
 module.exports.makeSureAuthenticated = (req, res, next) => {
   res.status(200).json({
     status: 'success',
