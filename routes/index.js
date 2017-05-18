610920f0099e9857b7d5d9c3a826d705e533ac5b
Jufebrown <jufebrown@gmail.com>
2017-05-18 12:20:11 -0500

    changed urls for release

--- routes/index.js
+++ routes/index.js
@@ -10,17 +10,17 @@ router.use(require('./words-route'))
 
 router.get('/', (req,res) => {
   res.json ({
-    "login": "/api/v1/auth/login",
-    "register": "/api/v1/auth/register",
-    "getLoggedInUser": "/api/v1/auth/user",
-    "getGamesForLoggedInUser": "/api/v1/user/games",
-    "getFriendsForLoggedInUser": "/api/v1/user/friends",
-    "getAllgames": "/api/v1/games",
-    "getWord" : "/api/v1/word/:<correct_word>",
-    "addWord": "/api/v1/words/new",
-    "addGame": "/api/v1/games/new",
-    "updateGame": "/api/v1/games/:<gameId>",
-    "getWordsForSpecifiedGame": "/api/v1/games/words?gameId=<gameId>"
+    "login": "https://warm-harbor-25906.herokuapp.com/api/v1/auth/login",
+    "register": "https://warm-harbor-25906.herokuapp.com/api/v1/auth/register",
+    "getLoggedInUser": "https://warm-harbor-25906.herokuapp.com/api/v1/auth/user",
+    "getGamesForLoggedInUser": "https://warm-harbor-25906.herokuapp.com/api/v1/user/games",
+    "getFriendsForLoggedInUser": "https://warm-harbor-25906.herokuapp.com/api/v1/user/friends",
+    "getAllgames": "https://warm-harbor-25906.herokuapp.com/api/v1/games",
+    "getWord" : "https://warm-harbor-25906.herokuapp.com/api/v1/word/:<correct_word>",
+    "addWord": "https://warm-harbor-25906.herokuapp.com/api/v1/words/new",
+    "addGame": "https://warm-harbor-25906.herokuapp.com/api/v1/games/new",
+    "updateGame": "https://warm-harbor-25906.herokuapp.com/api/v1/games/:<gameId>",
+    "getWordsForSpecifiedGame": "https://warm-harbor-25906.herokuapp.com/api/v1/games/words?gameId=<gameId>"
   })
 })
 
